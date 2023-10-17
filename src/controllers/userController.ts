794cdcb176375d7d3c4095eb3acd7b2a98997aff
Arthur Silva <73958393+ArthurMTS@users.noreply.github.com>
2023-10-17 15:39:26 -0300

    feat: subindo branch para tarefa de Listagem e Exclusão de CV

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,29 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
 
-async function Create(request: FastifyRequest, reply: FastifyReply) {}
+import { prisma } from "../config/db";
+
+async function Create(request: FastifyRequest, reply: FastifyReply) {
+  const BodySchema = z.object({
+    email: z.string().email(),
+    name: z.string(),
+    avatar: z.string(),
+    password: z.string(),
+  });
+
+  const { name, email, avatar, password } = BodySchema.parse(request.body);
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      avatar,
+      password,
+    },
+  });
+
+  return reply.status(201).send(user.id);
+}
 
 async function List(request: FastifyRequest, reply: FastifyReply) {}
 
