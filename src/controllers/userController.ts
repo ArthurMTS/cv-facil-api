28b89de6cc14b1d9226fd07ef41b6e5b94af9d8b
MarcosVini9999 <marcosviniciusandradedesousa@hotmail.com>
2023-10-20 20:06:50 -0300

    feat: user create delete show list

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,11 +1,45 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { prisma } from "../config/db";
+import { ParamsSchema } from "../config/zod";
 
-async function Create(request: FastifyRequest, reply: FastifyReply) {}
+async function Create(request: FastifyRequest, reply: FastifyReply) {
+  const data = request.body as any;
 
-async function List(request: FastifyRequest, reply: FastifyReply) {}
+  const user = await prisma.user.create({
+    data,
+  });
+}
+
+async function List(request: FastifyRequest, reply: FastifyReply) {
+  const users = await prisma.user.findMany();
+
+  reply.send(users);
+}
+
+async function Show(request: FastifyRequest, reply: FastifyReply) {
+  const { id } = ParamsSchema.parse(request.params);
+
+  const user = await prisma.user.findUnique({
+    where: { id },
+  });
+
+  if (!user) {
+    return reply.status(404).send();
+  }
+
+  reply.send(user);
+}
 
 async function Update(request: FastifyRequest, reply: FastifyReply) {}
 
-async function Delete(request: FastifyRequest, reply: FastifyReply) {}
+async function Delete(request: FastifyRequest, reply: FastifyReply) {
+  const { id } = ParamsSchema.parse(request.params);
 
-export { Create, List, Update, Delete };
+  await prisma.user.delete({
+    where: { id },
+  });
+
+  reply.status(204).send();
+}
+
+export { Create, List, Update, Delete, Show };
